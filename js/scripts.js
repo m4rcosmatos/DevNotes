9d59ec326023d46c4b0b638173c368e57b8dcf6d
m4rcosmatos <marcosmattos026@gmail.com>
2024-07-02 16:15:25 -0300

    Implementaçao das funcionalidades dos botoes de fixar, excluir, e duplicar

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,17 @@ const addNoteBtn = document.querySelector(".add-note")
 // Funcoes
 
 function showNotes(){
+    clearNotes();
+
     getNotes().forEach((note)=>{
         const noteElement = createNote(note.id, note.content, note.fixed)
         
         notesContainer.appendChild(noteElement)
     })
+}
+
+function clearNotes() {
+    notesContainer.replaceChildren([])
 }
 
 function addNote(){
@@ -46,14 +52,91 @@ function createNote(id, content, fixed) {
     textarea.placeholder = "Adicione algum texto..."
     element.appendChild(textarea)
 
+    const pinIcon = document.createElement("i")
+
+    pinIcon.classList.add(...["bi","bi-pin"])
+
+    element.appendChild(pinIcon)
+
+    const deleteIcon = document.createElement("i")
+
+    deleteIcon.classList.add(...["bi","bi-x-lg"])
+
+    element.appendChild(deleteIcon)
+
+    const duplicateIcon = document.createElement("i")
+
+    duplicateIcon.classList.add(...["bi","bi-file-earmark-plus"])
+
+    element.appendChild(duplicateIcon)
+
+    if(fixed){
+        element.classList.add("fixed")
+    }
+
+    // Eventos do elemento
+    element.querySelector(".bi-pin").addEventListener("click", () =>{
+        toggleFixNote(id)
+    })
+
+    element.querySelector(".bi-x-lg").addEventListener("click", () =>{
+        deleteNote(id,element)
+    })
+
+    element.querySelector(".bi-file-earmark-plus").addEventListener("click", () =>{
+        copyNote(id)
+    })
+
+
     return element;
+}
+
+function toggleFixNote(id){
+    const notes = getNotes();
+
+    const targetNote = notes.filter((note) => note.id ===id)[0];
+
+    targetNote.fixed = !targetNote.fixed;
+    
+    saveNotes(notes);
+
+    showNotes();
+}
+
+function deleteNote(id,element){
+    const notes = getNotes().filter((note) => note.id !== id)
+
+    saveNotes(notes)
+
+    notesContainer.removeChild(element);
+}
+
+function copyNote(id){
+    const notes = getNotes()
+
+    const targetNote = notes.filter((note)=> note.id === id)[0]
+
+    const noteObject = {
+        id: generateId(),
+        content: targetNote.content,
+        fixed:false,
+    }
+
+    const noteElement = createNote(noteObject.id, noteObject.content, noteObject.fixed)
+
+    notesContainer.appendChild(noteElement)
+
+    notes.push(noteObject)
+
+    saveNotes(notes)
 }
 
 // Local storage
 function getNotes(){
     const notes = JSON.parse(localStorage.getItem("notes") || "[]")
 
-    return notes;
+    const orderedNotes = notes.sort((a,b)=>a.fixed > b.fixed ? -1: 1)
+    return orderedNotes;
 }
 
 function saveNotes(notes){
